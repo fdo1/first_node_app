b3def403f2317136d2a227e845c95f87f0c7e1a7
Fernando Perez <fernando.madser@gmail.com>
2018-12-09 14:57:51 -0600

    Get request path

---
+++ index.js
@@ -0,0 +1,26 @@
+// Dependencies
+var http = require('http');
+var url = require('url');
+
+// The server should to all requests with a string
+var server = http.createServer(function(request, response){
+  // Get the URL and parse it
+  var parsedUrl = url.parse(request.url, true);
+
+  // Get the path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Send the response
+  response.end('Hello World!');
+
+  // Log the request path
+  console.log('Request received on path: ' + trimmedPath);
+
+});
+
+// Start the server and have it listen on port 3000
+
+server.listen(3000, function(){
+  console.log('The server is listening on port 3000 now');
+});
