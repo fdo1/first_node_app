5638145b9823551650d573a37b60b4c1be16724a
Fernando Perez <fernando.madser@gmail.com>
2018-12-09 19:50:38 -0600

    Get request payloads

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Dependencies
 var http = require('http');
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should to all requests with a string
 var server = http.createServer(function(request, response){
@@ -20,11 +21,21 @@ var server = http.createServer(function(request, response){
   // Get the headers as an object
   var headers = request.headers;
 
-  // Send the response
-  response.end('Hello World!');
+  // Get the payload, if any
+  var decoder = new StringDecoder('UTF-8');
+  var buffer = '';
+  request.on('data', function(data){
+    buffer += decoder.write(data);
+  });
+  request.on('end', function(){
+    buffer += decoder.end();
+    // Send the response
+    response.end('Hello World!');
 
-  // Log the request path
-  console.log('Request received with these headers: ', headers);
+    // Log the request path
+    console.log('Request received with this payload: ', buffer);
+  });
+
 
 });
 
