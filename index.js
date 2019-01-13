c0785b84775f9a531c8e4438ed6e1937716ab920
Fernando <fernando.madser@gmail.com>
2019-01-12 21:24:13 -0600

    Implements background workers. It also refactors the code and moves the server functionality to its own file.

--- index.js
+++ index.js
@@ -1,111 +1,22 @@
 // Dependencies
-var http = require('http');
-var https = require('https');
-var url = require('url');
-var config = require('./lib/config');
-var StringDecoder = require('string_decoder').StringDecoder;
-var fs = require('fs'); // This is the File System
-var _data = require('./lib/data');
-var handlers = require('./lib/handlers');
-var helpers = require('./lib/helpers');
 
-// @TODO get rid of this
-helpers.sendTwilioSms('5520673769', 'Hello', function(error) {
-  console.log(error);
-});
+var server = require('./lib/server');
+var workers = require('./lib/workers');
 
-// Instantiate the HTTP server
-var httpServer = http.createServer(function(request, response){
-  unifiedServer(request, response);
-});
-// Start the server and have it listen on port 3000
-httpServer.listen(config.httpPort, function(){
-  console.log('The server is listening on port ' + config.httpPort);
-});
+// Declare the app
+var app = {};
 
-// Instantiate the HTTPS server
-var httpsServerOptions = {
-  'key': fs.readFileSync('./https/key.pem'),
-  'cert': fs.readFileSync('./https/cert.pem')
-}
-var httpsServer = https.createServer(httpsServerOptions, function(request, response){
-  unifiedServer(request, response);
-});
-// Start the HTTPS server
-httpsServer.listen(config.httpsPort, function(){
-  console.log('The server is listening on port ' + config.httpsPort);
-});
+// Initialize the app
+app.init = function() {
+  // Start the server 
+  server.init();
 
-// Server logic for the HTTP sever and the HTTPS server
-var unifiedServer = function(request, response) {
-  // Get the URL and parse it
-  var parsedUrl = url.parse(request.url, true);
+  // Start workers
+  workers.init();
+};
 
-  // Get the path
-  var path = parsedUrl.pathname;
-  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+// Execute
+app.init();
 
-  // Get the query string as an object
-  var queryStringObject = parsedUrl.query;
-
-  // Get the HTTP method
-  var method = request.method.toLowerCase();
-
-  // Get the headers as an object
-  var headers = request.headers;
-
-  // Get the payload, if any
-  var decoder = new StringDecoder('UTF-8');
-  var buffer = '';
-  request.on('data', function(data){
-    buffer += decoder.write(data);
-  });
-  request.on('end', function(){
-    buffer += decoder.end();
-
-    // Choose the handler this request should go to. If one is not found, use the NotFound handler
-    var choesenHandler = typeof(router[trimmedPath]) !== 'undefined' ?
-      router[trimmedPath] : handlers.notFound;
-
-    // Construct the data object to send to the handler
-    var data = {
-      'trimmedPath' : trimmedPath,
-      'queryStringObject': queryStringObject,
-      'method': method,
-      'headers': headers,
-      'payload': helpers.parseJsonToObject(buffer)
-    };
-
-    // Route the request to the handler specified in the handler
-    choesenHandler(data, function(statusCode, payload){
-      // Use the status code called back by the handler, or default to 200
-      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-
-      // Use the payload called back by the handler, or default to an empty object
-      console.log((payload));
-      paylod = typeof(payload) == 'object' ? payload : {};
-
-      // Convert the payload to string
-      var payloadString = JSON.stringify(payload);
-
-      // Return the response
-      response.setHeader('Content-Type', 'application/json');
-      response.writeHead(statusCode);
-      response.end(payloadString);
-
-      // Log the request path
-      console.log('Returning this response: ', statusCode, payloadString);
-    });
-
-  });
-}
-
-
- 
-// Define a request router
-var router = {
-  'ping': handlers.ping,
-  'users': handlers.users,
-  'tokens': handlers.tokens,
-  'checks': handlers.checks
-}
+// Export the app
+module.exports = app;
